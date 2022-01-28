57b80efcfcc088033cb2622f1b5d42f58de7c872
ziomciopoziomcio <60813479+ziomciopoziomcio@users.noreply.github.com>
2022-01-28 10:47:16 +0100

    Translation improvement and graphical changes

--- Programmes/list_sorting.py
+++ Programmes/list_sorting.py
@@ -1,22 +1,25 @@
-#Declaration of variable values
+# Declaration of variable values
 x = float(0)
 position = 1
 list = []
 
-#Function definition
+# Function definition
+
+
 def floatcheck(x):
     try:
         float(x)
     except ValueError:
         return False
 
-#Presentation of the programme guidelines
+
+# Presentation of the programme guidelines
 print("The program sorts the numbers entered by the user")
 print("The program accepts integers and decimals")
 print("NOTE: Use a dot NOT a comma to enter a decimal number!")
 print("To stop entering numbers, enter any value other than a whole number or decimal point")
 
-#Dopisywanie wartości do listy
+# Inserting values into a list
 while type(x) == float or type(x) == int:
     print("Give the number ", position, ":")
     x = input()
@@ -28,9 +31,9 @@ while type(x) == float or type(x) == int:
         list.append(x)
     position = position + 1
 
-#Sorting list and returning results
+# Sorting list and returning results
 list.sort()
 print(list)
 
-#Close of the programme
+# Close of the programme
 input("Press ENTER to close the program")

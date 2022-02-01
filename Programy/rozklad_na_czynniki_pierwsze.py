9ae302191574485fd1565158473688f6286167f7
ziomciopoziomcio <60813479+ziomciopoziomcio@users.noreply.github.com>
2022-02-01 10:23:33 +0100

    Aktualizacja programu Rozkład na czynniki pierwsze
    Dodanie funkcji pobierania wartości

--- Programy/rozklad_na_czynniki_pierwsze.py
+++ Programy/rozklad_na_czynniki_pierwsze.py
@@ -1,2 +1,36 @@
-#Określanie zmiennych 
-n = 0
+
+#Definicja funkcji
+def intcheck(check):
+    try:
+        int(check)
+    except ValueError:
+        return False
+    else:
+        return True
+
+#Pierwsze pobranie n i sprawdzenie czy jest liczbą naturalną
+n = input("Podaj liczbę do rozkładu na czynniki pierwsze")
+
+if intcheck(n) == True:
+    n = int(n)
+    if n >= 1:
+        ncheck = True
+    else:
+        ncheck = False
+else:
+    ncheck = False
+    
+#Pobieranie n i sprawdzanie czy jest liczbą naturalną do momentu gdy użytkownik wprowadzi liczbę naturalną
+while ncheck == False:
+    print("Podane dane nie są liczbą naturalną")
+    n = input("Podaj liczbę do rozkładu na czynniki pierwsze")
+    if intcheck(n) == True:
+        n = int(n)
+        if n >= 1:
+            ncheck = True
+        else:
+            ncheck = False
+    else:
+        ncheck = False
+
+print(n)

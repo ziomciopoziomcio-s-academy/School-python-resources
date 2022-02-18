fecce8bec4afcc2933b2d5f7e59f7177c0ee2f9d
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2022-02-18 20:41:59 +0100

    Finish the script that downloads the current list of available programs.

--- launcher/updater.py
+++ launcher/updater.py
@@ -2,7 +2,16 @@ import requests
 list_url = "https://raw.githubusercontent.com/ziomciopoziomcio-s-academy/School-python-resources/Launcher/launcher/programs.txt"
 local = "lts.txt"
 r = requests.get(list_url)
-with open(local, 'wb')as file:
+with open(local, 'wb') as file:
     file.write(r.content)
+    file.close()
 
+def programs_list():
+    list = []
+    with open(local, 'r') as file:
+        for line in file:
+            line = line.strip()
+            list.append(line)
+    return list
 
+test = programs_list()

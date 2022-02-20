23fcd92d21ab70659cb5cdc45e463cac8a027edd
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2022-02-20 17:49:13 +0100

    Added program version check

--- gui.py
+++ gui.py
@@ -2,9 +2,22 @@
 from appJar import gui
 app = gui()
 from Launcher.updater import *
+version = "0.0.1 PA"
+update()
 
-#Available programs list creation
+#Update
 available_programs = programs_list()
+lts_ver = version_check()
+print(lts_ver)
+app.startSubWindow("Update available!")
+app.addLabel("l1", "Your program version:")
+app.addLabel("l2", version)
+app.addLabel("l3", "Latest version of the program:")
+app.addLabel("l4", lts_ver)
+app.addLabel("l5", "Update your program {{here}}") #TO DO
+app.stopSubWindow()
+if version != lts_ver:
+    app.showSubWindow("Update available!")
 
 #Title
 app.addLabel("title", "Welcome in the School Python Resources")
--- launcher/updater.py
+++ launcher/updater.py
@@ -5,16 +5,16 @@ ver_list = "https://raw.githubusercontent.com/ziomciopoziomcio-s-academy/School-
 local_ver = "lts_ver.txt"
 l = requests.get(list_url)
 v = requests.get(ver_list)
-
-with open(local_prg, 'wb') as file:
-    file.write(l.content)
-    file.close()
-
-with open(local_ver, 'wb') as file:
-    file.write(v.content)
-    file.close()
+def update():
+    with open(local_prg, 'wb') as file:
+        file.write(l.content)
+        file.close()
+    with open(local_ver, 'wb') as file:
+        file.write(v.content)
+        file.close()
 
 def programs_list():
+    update()
     list = []
     with open(local_prg, 'r') as file:
         for line in file:
@@ -23,5 +23,7 @@ def programs_list():
     return list
 
 def version_check():
+    update()
     with open(local_ver, 'r') as file:
-        return file.read().strip()
+        ver = file.read().strip()
+    return ver

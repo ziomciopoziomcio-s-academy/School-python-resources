7d0e267967c0618f16c124acf9a8943dfcc78bf8
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2022-02-20 15:51:58 +0100

    Update updater.py
    Adjusting code to add program version checking functionality

--- launcher/updater.py
+++ launcher/updater.py
@@ -1,17 +1,15 @@
 import requests
 list_url = "https://raw.githubusercontent.com/ziomciopoziomcio-s-academy/School-python-resources/Launcher/launcher/programs.txt"
-local = "lts.txt"
+local_prg = "lts_prg.txt"
 r = requests.get(list_url)
-with open(local, 'wb') as file:
+with open(local_prg, 'wb') as file:
     file.write(r.content)
     file.close()
 
 def programs_list():
     list = []
-    with open(local, 'r') as file:
+    with open(local_prg, 'r') as file:
         for line in file:
             line = line.strip()
             list.append(line)
     return list
-
-test = programs_list()
